5474f21e7e288dc11e647ff3a96250ee73a4411b
xlem <xlem@akkoro.io>
2020-10-04 16:33:18 -0230

    mv rpc code to core;upd for new macros;add crypto

---
+++ crypto/src/main.rs
@@ -0,0 +1,19 @@
+use assemblylift_core_iomod::iomod;
+use futures::future::BoxFuture;
+use tokio;
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() {
+    iomod!(akkoro.std.crypto => {
+        uuid4 => uuid4
+    });
+}
+
+fn uuid4(_input: Vec<u8>) -> BoxFuture<'static, Vec<u8>> {
+    Box::pin(async move {
+        let buffer = &mut Uuid::encode_buffer();
+        let id = Uuid::new_v4().to_hyphenated().encode_lower(buffer);
+        Vec::from(id.as_bytes())
+    })
+}
