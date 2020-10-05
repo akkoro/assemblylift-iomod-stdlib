8733db6ef7f427581a70fd81825f609e3d8d68f9
xlem <xlem@akkoro.io>
2020-10-05 00:03:59 -0230

    use serde to serialize uuid4 output

--- crypto/src/main.rs
+++ crypto/src/main.rs
@@ -3,6 +3,8 @@ use futures::future::BoxFuture;
 use tokio;
 use uuid::Uuid;
 
+use serde_json::to_vec;
+
 #[tokio::main]
 async fn main() {
     iomod!(akkoro.std.crypto => {
@@ -14,6 +16,6 @@ fn uuid4(_input: Vec<u8>) -> BoxFuture<'static, Vec<u8>> {
     Box::pin(async move {
         let buffer = &mut Uuid::encode_buffer();
         let id = Uuid::new_v4().to_hyphenated().encode_lower(buffer);
-        Vec::from(id.as_bytes())
+        to_vec(&String::from(id)).unwrap()
     })
 }
