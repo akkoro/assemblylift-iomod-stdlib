ca37caeae39a8ee1eec8d0e614be588293b9711e
xlem <xlem@akkoro.io>
2020-10-31 01:18:32 -0230

    implement remaining dynamodb calls

--- dynamodb/guest/src/lib.rs
+++ dynamodb/guest/src/lib.rs
@@ -4,10 +4,7 @@ use assemblylift_core_iomod_guest::{call, export_iomod_guest};
 use serde::export::Formatter;
 use serde::{Deserialize, Serialize};
 
-use crate::structs::{
-    DeleteItemInput, DeleteItemOutput, GetItemInput, GetItemOutput, ListTablesInput,
-    ListTablesOutput, PutItemInput, PutItemOutput, UpdateItemInput, UpdateItemOutput,
-};
+use crate::structs::*;
 
 export_iomod_guest!(akkoro, aws, dynamodb);
 
@@ -25,11 +22,47 @@ impl fmt::Display for Error {
 }
 impl std::error::Error for Error {}
 
-call!(list_tables, ListTablesInput => Result<ListTablesOutput, Error>);
-call!(put_item,    PutItemInput    => Result<PutItemOutput, Error>);
-call!(get_item,    GetItemInput    => Result<GetItemOutput, Error>);
-call!(delete_item, DeleteItemInput => Result<DeleteItemOutput, Error>);
-call!(update_item, UpdateItemInput => Result<UpdateItemOutput, Error>);
+call!(batch_get_item, BatchGetItemInput => Result<BatchGetItemInput, Error>);
+call!(batch_write_item, BatchWriteItemInput => Result<BatchWriteItemInput, Error>);
+call!(create_backup, CreateBackupInput => Result<CreateBackupInput, Error>);
+call!(create_global_table, CreateGlobalTableInput => Result<CreateGlobalTableInput, Error>);
+call!(create_table, CreateTableInput => Result<CreateTableInput, Error>);
+call!(delete_backup, DeleteBackupInput => Result<DeleteBackupInput, Error>);
+call!(delete_item, DeleteItemInput => Result<DeleteItemInput, Error>);
+call!(delete_table, DeleteTableInput => Result<DeleteTableInput, Error>);
+call!(describe_backup, DescribeBackupInput => Result<DescribeBackupInput, Error>);
+call!(describe_continuous_backups, DescribeContinuousBackupsInput => Result<DescribeContinuousBackupsInput, Error>);
+call!(describe_contributor_insights, DescribeContributorInsightsInput => Result<DescribeContributorInsightsInput, Error>);
+call!(describe_endpoints, DescribeEndpointsRequest => Result<DescribeEndpointsRequest, Error>);
+call!(describe_global_table, DescribeGlobalTableInput => Result<DescribeGlobalTableInput, Error>);
+call!(describe_global_table_settings, DescribeGlobalTableSettingsInput => Result<DescribeGlobalTableSettingsInput, Error>);
+call!(describe_limits, DescribeLimitsInput => Result<DescribeLimitsInput, Error>);
+call!(describe_table, DescribeTableInput => Result<DescribeTableInput, Error>);
+call!(describe_table_replica_auto_scaling, DescribeTableReplicaAutoScalingInput => Result<DescribeTableReplicaAutoScalingInput, Error>);
+call!(describe_time_to_live, DescribeTimeToLiveInput => Result<DescribeTimeToLiveInput, Error>);
+call!(get_item, GetItemInput => Result<GetItemInput, Error>);
+call!(list_backups, ListBackupsInput => Result<ListBackupsInput, Error>);
+call!(list_contributor_insights, ListContributorInsightsInput => Result<ListContributorInsightsInput, Error>);
+call!(list_global_tables, ListGlobalTablesInput => Result<ListGlobalTablesInput, Error>);
+call!(list_tables, ListTablesInput => Result<ListTablesInput, Error>);
+call!(list_tags_of_resource, ListTagsOfResourceInput => Result<ListTagsOfResourceInput, Error>);
+call!(put_item, PutItemInput => Result<PutItemInput, Error>);
+call!(query, QueryInput => Result<QueryInput, Error>);
+call!(restore_table_from_backup, RestoreTableFromBackupInput => Result<RestoreTableFromBackupInput, Error>);
+call!(restore_table_to_point_in_time, RestoreTableToPointInTimeInput => Result<RestoreTableToPointInTimeInput, Error>);
+call!(scan, ScanInput => Result<ScanInput, Error>);
+call!(tag_resource, TagResourceInput => Result<TagResourceInput, Error>);
+call!(transact_get_items, TransactGetItemsInput => Result<TransactGetItemsInput, Error>);
+call!(transact_write_items, TransactWriteItemsInput => Result<TransactWriteItemsInput, Error>);
+call!(untag_resource, UntagResourceInput => Result<UntagResourceInput, Error>);
+call!(update_continuous_backups, UpdateContinuousBackupsInput => Result<UpdateContinuousBackupsInput, Error>);
+call!(update_contributor_insights, UpdateContributorInsightsInput => Result<UpdateContributorInsightsInput, Error>);
+call!(update_global_table, UpdateGlobalTableInput => Result<UpdateGlobalTableInput, Error>);
+call!(update_global_table_settings, UpdateGlobalTableSettingsInput => Result<UpdateGlobalTableSettingsInput, Error>);
+call!(update_item, UpdateItemInput => Result<UpdateItemInput, Error>);
+call!(update_table, UpdateTableInput => Result<UpdateTableInput, Error>);
+call!(update_table_replica_auto_scaling, UpdateTableReplicaAutoScalingInput => Result<UpdateTableReplicaAutoScalingInput, Error>);
+call!(update_time_to_live, UpdateTimeToLiveInput => Result<UpdateTimeToLiveInput, Error>);
 
 #[macro_export]
 macro_rules! val {
--- dynamodb/src/main.rs
+++ dynamodb/src/main.rs
@@ -27,11 +27,47 @@ static DYNAMODB: Lazy<DynamoDbClient> = Lazy::new(|| {
 #[tokio::main]
 async fn main() {
     iomod!(akkoro.aws.dynamodb => {
-        list_tables => list_tables,
-        put_item => put_item,
-        get_item => get_item,
+        batch_get_item => batch_get_item,
+        batch_write_item => batch_write_item,
+        create_backup => create_backup,
+        create_global_table => create_global_table,
+        create_table => create_table,
+        delete_backup => delete_backup,
         delete_item => delete_item,
-        update_item => update_item
+        delete_table => delete_table,
+        describe_backup => describe_backup,
+        describe_continuous_backups => describe_continuous_backups,
+        describe_contributor_insights => describe_contributor_insights,
+        describe_endpoints => describe_endpoints,
+        describe_global_table => describe_global_table,
+        describe_global_table_settings => describe_global_table_settings,
+        describe_limits => describe_limits,
+        describe_table => describe_table,
+        describe_table_replica_auto_scaling => describe_table_replica_auto_scaling,
+        describe_time_to_live => describe_time_to_live,
+        get_item => get_item,
+        list_backups => list_backups,
+        list_contributor_insights => list_contributor_insights,
+        list_global_tables => list_global_tables,
+        list_tables => list_tables,
+        list_tags_of_resource => list_tags_of_resource,
+        put_item => put_item,
+        query => query,
+        restore_table_from_backup => restore_table_from_backup,
+        restore_table_to_point_in_time => restore_table_to_point_in_time,
+        scan => scan,
+        tag_resource => tag_resource,
+        transact_get_items => transact_get_items,
+        transact_write_items => transact_write_items,
+        untag_resource => untag_resource,
+        update_continuous_backups => update_continuous_backups,
+        update_contributor_insights => update_contributor_insights,
+        update_global_table => update_global_table,
+        update_global_table_settings => update_global_table_settings,
+        update_item => update_item,
+        update_table => update_table,
+        update_table_replica_auto_scaling => update_table_replica_auto_scaling,
+        update_time_to_live => update_time_to_live
     });
 }
 
@@ -57,8 +93,65 @@ macro_rules! dynamodb {
     };
 }
 
-dynamodb!(list_tables, ListTablesOutput);
-dynamodb!(put_item,    PutItemOutput);
-dynamodb!(get_item,    GetItemOutput);
+macro_rules! dynamodb_noinput {
+    ($call:ident, $output:ty) => {
+        fn $call(_input: Vec<u8>) -> BoxFuture<'static, Vec<u8>> {
+            Box::pin(async move {
+                use rusoto_dynamodb::*;
+                match DYNAMODB.$call().await {
+                    Ok(result) => {
+                        serde_json::to_vec(&Result::<$output, guest::Error>::Ok(result)).unwrap()
+                    }
+                    Err(err) => {
+                        serde_json::to_vec(&Result::<$output, guest::Error>::Err(guest::Error {
+                            why: err.to_string(),
+                        }))
+                        .unwrap()
+                    }
+                }
+            })
+        }
+    };
+}
+
+dynamodb!(batch_get_item, BatchGetItemOutput);
+dynamodb!(batch_write_item, BatchWriteItemOutput);
+dynamodb!(create_backup, CreateBackupOutput);
+dynamodb!(create_global_table, CreateGlobalTableOutput);
+dynamodb!(create_table, CreateTableOutput);
+dynamodb!(delete_backup, DeleteBackupOutput);
 dynamodb!(delete_item, DeleteItemOutput);
+dynamodb!(delete_table, DeleteTableOutput);
+dynamodb!(describe_backup, DescribeBackupOutput);
+dynamodb!(describe_continuous_backups, DescribeContinuousBackupsOutput);
+dynamodb!(describe_contributor_insights, DescribeContributorInsightsOutput);
+dynamodb_noinput!(describe_endpoints, DescribeEndpointsResponse);
+dynamodb!(describe_global_table, DescribeGlobalTableOutput);
+dynamodb!(describe_global_table_settings, DescribeGlobalTableSettingsOutput);
+dynamodb_noinput!(describe_limits, DescribeLimitsOutput);
+dynamodb!(describe_table, DescribeTableOutput);
+dynamodb!(describe_table_replica_auto_scaling, DescribeTableReplicaAutoScalingOutput);
+dynamodb!(describe_time_to_live, DescribeTimeToLiveOutput);
+dynamodb!(get_item, GetItemOutput);
+dynamodb!(list_backups, ListBackupsOutput);
+dynamodb!(list_contributor_insights, ListContributorInsightsOutput);
+dynamodb!(list_global_tables, ListGlobalTablesOutput);
+dynamodb!(list_tables, ListTablesOutput);
+dynamodb!(list_tags_of_resource, ListTagsOfResourceOutput);
+dynamodb!(put_item, PutItemOutput);
+dynamodb!(query, QueryOutput);
+dynamodb!(restore_table_from_backup, RestoreTableFromBackupOutput);
+dynamodb!(restore_table_to_point_in_time, RestoreTableToPointInTimeOutput);
+dynamodb!(scan, ScanOutput);
+dynamodb!(tag_resource, ());
+dynamodb!(transact_get_items, TransactGetItemsOutput);
+dynamodb!(transact_write_items, TransactWriteItemsOutput);
+dynamodb!(untag_resource, ());
+dynamodb!(update_continuous_backups, UpdateContinuousBackupsOutput);
+dynamodb!(update_contributor_insights, UpdateContributorInsightsOutput);
+dynamodb!(update_global_table, UpdateGlobalTableOutput);
+dynamodb!(update_global_table_settings, UpdateGlobalTableSettingsOutput);
 dynamodb!(update_item, UpdateItemOutput);
+dynamodb!(update_table, UpdateTableOutput);
+dynamodb!(update_table_replica_auto_scaling, UpdateTableReplicaAutoScalingOutput);
+dynamodb!(update_time_to_live, UpdateTimeToLiveOutput);
