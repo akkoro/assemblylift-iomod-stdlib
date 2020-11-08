1ca28a444de7b281f3bb33ca3c78df408ea76b3a
xlem <xlem@akkoro.io>
2020-11-08 14:57:01 -0330

    refactor macros

---
+++ modules/s3/src/macros.rs
@@ -0,0 +1,51 @@
+#[macro_export]
+macro_rules! s3 {
+    ($call:ident, $output:ty) => {
+        fn $call(input: Vec<u8>) -> BoxFuture<'static, Vec<u8>> {
+            Box::pin(async move {
+                use rusoto_s3::*;
+                let deserialized = serde_json::from_slice(input.as_slice()).unwrap();
+                $crate::__s3!($call, $output)
+            })
+        }
+    };
+    ($call:ident, $input:ty => $output:ty) => {
+        fn $call(input: Vec<u8>) -> BoxFuture<'static, Vec<u8>> {
+            Box::pin(async move {
+                use rusoto_s3::*;
+                let deserialized: $input = serde_json::from_slice(input.as_slice()).unwrap();
+                $crate::__s3!($call, $output)
+            })
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! s3_noinput {
+    ($call:ident, $output:ty) => {
+        fn $call(_input: Vec<u8>) -> BoxFuture<'static, Vec<u8>> {
+            Box::pin(async move {
+                use rusoto_s3::*;
+                $crate::__s3!($call, $output)
+            })
+        }
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __s3 {
+    ($call:ident, $output:ty) => {
+        match S3.$call(deserialized).await {
+            Ok(result) => {
+                serde_json::to_vec(&Result::<$output, guest::Error>::Ok(result)).unwrap()
+            }
+            Err(err) => {
+                serde_json::to_vec(&Result::<$output, guest::Error>::Err(guest::Error {
+                    why: err.to_string(),
+                }))
+                .unwrap()
+            }
+        }
+    }
+}
