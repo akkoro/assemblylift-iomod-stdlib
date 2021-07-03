92df9ad7e04afcdc36b604aeff7d95550aeea0c5
xlem <xlem@akkoro.io>
2021-07-03 19:33:59 -0230

    debug s3 list_objects_v2

--- modules/s3/src/main.rs
+++ modules/s3/src/main.rs
@@ -5093,6 +5093,9 @@ fn __list_objects_v2(input: ListObjectsV2Request) -> BoxFuture<'static, Vec<u8>>
                         serde_json::to_vec(&Result::<ListObjectsV2Output, guest::Error>::Ok(output)).unwrap()
                     }
                     status => {
+                        let body = &*hyper::body::to_bytes(response.into_body()).await.unwrap();
+                        let body = String::from(std::str::from_utf8(body).unwrap());
+                        println!("DEBUG: response={}", body);
                         serde_json::to_vec(&Result::<ListObjectsV2Output, guest::Error>::Err(guest::Error {
                             why: String::from(status.canonical_reason().unwrap()),
                         }))
