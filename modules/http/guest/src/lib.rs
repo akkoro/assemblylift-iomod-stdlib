e3efdacb8a003979fb92d79d6bec1cf41bf7090c
xlem <xlem@akkoro.io>
2022-10-01 16:40:59 -0230

    add auth() to request builder

--- modules/http/guest/src/lib.rs
+++ modules/http/guest/src/lib.rs
@@ -3,6 +3,7 @@ pub mod structs;
 use std::collections::HashMap;
 use std::fmt;
 use std::fmt::Formatter;
+use std::iter::Map;
 
 use assemblylift_core_iomod_guest::{call, iomod};
 use serde::{Deserialize, Serialize};
@@ -65,6 +66,17 @@ impl HttpRequestBuilder {
 
     pub fn body(mut self, body: &str) -> HttpRequestBuilder {
         self.req.body = Some(body.into());
+        self
+    }
+
+    pub fn auth(mut self, r#type: &str, options: HashMap<&str, String>) -> HttpRequestBuilder {
+        self.req.auth = Some(HttpAuth {
+            r#type: r#type.to_string(),
+            service: match options.get("service") {
+                Some(s) => Some(s.clone()),
+                None => Some("execute-api".to_string()),
+            },
+        });
         self
     }
     
